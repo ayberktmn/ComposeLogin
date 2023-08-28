c672433fc4dc684df045a779d5c48964dd00d046
ayberktmn <ayberktemin@gmail.com>
2023-08-28 12:49:57 +0300

    Padding Image

--- app/src/main/java/com/example/composebase/FotografFragment.kt
+++ app/src/main/java/com/example/composebase/FotografFragment.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,21 +33,23 @@ fun FotografFragment() {
             fontSize = 20.sp
         )
         Row(
+            modifier = Modifier
+                .padding(top = 16.dp, bottom = 15.dp),
             horizontalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier.fillMaxWidth()
+
         ) {
             Image(
-                modifier = Modifier
-                    .padding(top = 16.dp, bottom = 15.dp),
+
                 painter = painterResource(R.drawable.android),
                 contentDescription = "Hoşgeldiniz resmi",
-
+                modifier = Modifier
+                    .size(200.dp)
             )
             Image(
-                modifier = Modifier
-                    .padding(top = 16.dp, bottom = 15.dp),
                 painter = painterResource(R.drawable.game),
                 contentDescription = "Hoşgeldiniz resmi",
+                modifier = Modifier
+                    .size(200.dp)
             )
         }
     }
